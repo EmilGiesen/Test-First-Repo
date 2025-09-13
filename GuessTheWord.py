8cbaf580b14adafc1512bf72be8f9a22f6d211b8
Emil Giesen <giesenemil@gmail.com>
2025-09-14 01:46:19 +0200

    Project Done

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -15,7 +15,7 @@ print("Whalecum to the game" + game_name)
 print("You have 5 trys to guess a 5 Letter word")
 
 while trys <= 5:
-    print("These Letters are in the Word that you know alreasy: " + str(missplaced_letters) + "\n These Letter are not in the Word: " + str(not_in_word))
+    print("These Letters are in the Word that you know alreasy: " + str(missplaced_letters) + "\nThese Letter are not in the Word: " + str(not_in_word))
     guess = input("Input Your " + str(trys) + " guess \n")
     guess = guess.strip() 
     guess = guess.lower()
@@ -26,6 +26,9 @@ while trys <= 5:
     elif guess == word:
         print("WOW you guess the Word correctly it was " + word + " you guessed it in " + str(trys) + " tries")
         break
+    elif trys == 5 and guess != word:
+        print("Whomp looks like you suck at this the word would have been " + word)
+        break
     else:
         trys += 1
     for x in range(0,5):
@@ -33,6 +36,8 @@ while trys <= 5:
         letter_guess_check = 0
         if guess[x] == word[x]:
             print("The " + str(x + 1) + " letter in your Guess is Correct")
+            missplaced_letters.append(guess[x])
+            missplaced_letters = list(dict.fromkeys(missplaced_letters))
         elif guess[x] in word:
             print("The " + str(x + 1) + " letter in your Guess is in the Word")
             missplaced_letters.append(guess[x])
@@ -41,7 +46,6 @@ while trys <= 5:
             not_in_word.append(guess[x])
             not_in_word = list(dict.fromkeys(not_in_word))
 
-print("Whomp looks like you suck at this the word would have been " + word)
 
                 
 
